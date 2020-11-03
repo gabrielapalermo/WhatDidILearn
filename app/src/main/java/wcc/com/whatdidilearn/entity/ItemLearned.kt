be78ac8a46965cc9a8ad9665a73fedebc38d7975
GABRIELA <palermogabrielaribeiro@hotmail.com>
2020-11-03 20:58:20 -0300

    rewriting entity with room

--- app/src/main/java/wcc/com/whatdidilearn/entity/ItemLearned.kt
+++ app/src/main/java/wcc/com/whatdidilearn/entity/ItemLearned.kt
@@ -1,7 +1,23 @@
 package wcc.com.whatdidilearn.entity
 
+import android.icu.text.IDNA
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Learned_item")
+
 data class ItemLearned (
+    @ColumnInfo(name="item_title")
     val title: String,
+
+    @ColumnInfo(name="item_description")
     val description: String,
-    val level: Level
+
+    @ColumnInfo(name="item_level")
+    val level: Level,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="item_id")
+    val id: Int = 0
 )
