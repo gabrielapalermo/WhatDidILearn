2be75479efac53051a8545bef849e868f4524d9d
GABRIELA <palermogabrielaribeiro@hotmail.com>
2020-11-03 19:52:22 -0300

    adding ViewBinding

--- app/src/main/java/wcc/com/whatdidilearn/view/MainActivity.kt
+++ app/src/main/java/wcc/com/whatdidilearn/view/MainActivity.kt
@@ -5,13 +5,15 @@ import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import wcc.com.whatdidilearn.R
 import wcc.com.whatdidilearn.data.DataBaseItems
+import wcc.com.whatdidilearn.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.items_recycle_view)
+        val recyclerView = binding.itemsRecycleView
         val adapter = LearnedItemsAdapter()
         recyclerView.adapter = adapter
 
